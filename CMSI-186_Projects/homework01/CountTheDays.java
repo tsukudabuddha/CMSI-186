e8856c994fa968d402469f695056875827a7fbab
Colin-Karch <35310102+Colin-Karch@users.noreply.github.com>
2018-01-24 23:06:27 -0800

    CountTheDays code

---
+++ CMSI-186_Projects/homework01/CountTheDays.java
@@ -0,0 +1,11 @@
+public class CountTheDays {
+  public static void man (String [] args) {
+    long month0 = Long.parseLong(args[0]);
+		long day0 = Long.parseLong(args[1]);
+		long year0 = Long.parseLong(args[2]);
+		long month1 = Long.parseLong(args[3]);
+		long day1 = Long.parseLong(args[4]);
+		long year1 = Long.parseLong(args[5]);
+		System.out.println(CalendarStuff.daysBetween(month0, day0, year0, month1, day1, year1));
+  }
+}
